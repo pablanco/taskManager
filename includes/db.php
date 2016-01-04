44b6f31b2c6f1f91eefe65a375000adac6bc8e4a
pablanco <pblanco@devxtend.com>
2016-01-04 14:32:23 -0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php 
 $DB_HOST = '127.0.0.1';
-$DB_USER = 'devexten_demo';
-$DB_PASS = 'Aw]@LTA@xB$k';
-$DB_NAME = 'devexten_task_manager';
+$DB_USER = '*****';
+$DB_PASS = '*****';
+$DB_NAME = '*****';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
